6249aa382deab0f9db897e6f999fb6f77de3cb09
Soahc <philipp@phillsoft.de>
2013-07-06 22:09:12 +0200

    ProductType

--- sharedLib/ejbModule/org/dieschnittstelle/jee/esa/erp/entities/ProductType.java
+++ sharedLib/ejbModule/org/dieschnittstelle/jee/esa/erp/entities/ProductType.java
@@ -10,4 +10,29 @@ public enum ProductType {
 	public static ProductType deserialise(String pt) {	
 		return ProductType.valueOf(ProductType.class,pt);
 	}
+	
+	public static String toReadableString(ProductType type){
+		switch (type) {
+		case BREAD:
+			return "Brot";
+		case ROLL:
+			return "Brötchen";
+		case PASTRY:
+			return "Kuchen";	
+		default:
+			return "unbekannt";
+		}
+	}
+	
+	public static ProductType fromReadableString (String str){
+		if (str.equals("Brot")){
+			return BREAD;
+		}else if (str.equals("Brötchen")){
+			return ROLL;
+		}else if (str.equals("Kuchen")){
+			return PASTRY;
+		}else{
+			return null;
+		}
+	}
 }
